fa180bcfcb91c1cfeede85ad9d72531a22c755f0
Verique <suolucitem@gmail.com>
2024-06-12 21:46:04 +0300

    new alias

--- .config/Microsoft.PowerShell_profile.ps1
+++ .config/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,13 @@
-function Edit-Nvim {
-    pushd C:\Users\suolu\AppData\Local\nvim
+function Config-Edit {
+    pushd $HOME/.config/
 }
-function nvr {if (-not (Test-Path "\\.\pipe\nvim-nvr")) { wezterm -e "nvim --listen "\\.\pipe\nvim-nvr" $args" } else
-		{ if ($args) {nvim --server "\\.\pipe\nvim-nvr" --remote $args } } }
+
+function nvr {
+    if (-not (Test-Path "\\.\pipe\nvim-nvr")) 
+        { wezterm -e "nvim --listen "\\.\pipe\nvim-nvr" $args" } 
+    else
+		{ if ($args) {nvim --server "\\.\pipe\nvim-nvr" --remote $args } } 
+}
 
 function ChromeCORSless {
     & "C:\Program Files\Google\Chrome\Application\chrome.exe" --disable-web-security --disable-gpu --user-data-dir=%LOCALAPPDATA%\Google\chromeTemp
